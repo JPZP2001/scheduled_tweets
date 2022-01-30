01c582e69c04905fcd898b3f2cc85d408ab82371
JP Zubieta <juan.zubieta@udem.edu>
2022-01-29 18:17:14 -0600

    video 22

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -14,4 +14,27 @@ class PasswordResetsController < ApplicationController
 
     end
 
+    def edit 
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+    rescue
+        redirect_to sign_in_path, alert: "Your token has expired. PLease try again."
+    end
+
+    def update
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: "Your password was reset succesfully. Please sign in."
+        else
+            render :edit
+        end
+
+
+    end
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
+
+
+
 end
